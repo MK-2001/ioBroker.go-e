4ab4c3f7dbe4d6a007ffcb02a0f93d1be909caf7
Marcus K <marcus-github@te-amor.de>
2024-10-27 15:45:05 +0100

    eslint migration to version 9

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("eslint:recommended"), {
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            ...globals.mocha,
+        },
+
+        ecmaVersion: 2018,
+        sourceType: "commonjs",
+    },
+
+    rules: {
+        indent: ["error", 4, {
+            SwitchCase: 1,
+        }],
+
+        "no-console": "off",
+        "no-var": "error",
+        "prefer-const": "error",
+
+        quotes: ["error", "double", {
+            avoidEscape: true,
+            allowTemplateLiterals: true,
+        }],
+
+        semi: ["error", "always"],
+    },
+}];
